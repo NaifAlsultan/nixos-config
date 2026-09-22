3564b4e54389430068b5a22e9a86c72e80c0848b
Naif Alsultan <contact@naifalsultan.com>
2026-09-23 01:22:29 +0300

    refine starship prompt

--- home.nix
+++ home.nix
@@ -146,20 +146,20 @@
     enableNushellIntegration = true;
     settings = {
       add_newline = false;
-      format = "$directory$git_branch\n$character";
+      format = "$directory$git_branch $character";
       character = {
         success_symbol = "[>](bold #53bf97)";
         error_symbol = "[>](bold #f08778)";
       };
       directory = {
-        format = "[ $path ]($style)";
+        format = "[ $path ]($style)[](fg:#60b8d6)";
         style = "bold fg:#1f2430 bg:#60b8d6";
         truncate_to_repo = false;
         truncation_length = 0;
       };
       git_branch = {
-        format = "[](fg:#60b8d6 bg:#53bf97)[  $branch ]($style)[](fg:#53bf97)";
-        style = "bold fg:#1f2430 bg:#53bf97";
+        format = " [  $branch ]($style)[](fg:#fdcc60)";
+        style = "bold fg:#1f2430 bg:#fdcc60";
         symbol = "";
       };
     };
